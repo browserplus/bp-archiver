56246fd59f3311868f2cc1167c8e8b0bc93e894d
Gary MacDonald <garymd@yahoo-inc.com>
2010-07-29 13:23:01 -0700

    cleanup

--- external/build.rb
+++ external/build.rb
@@ -2,9 +2,6 @@
 
 require "bakery/ports/bakery"
 
-# boost/zlib/bzip2 built from bakery port, bp-file is built from 
-# submodule source
-# libarchive will be put into ports/archivers/libarchive when 2.8.0 is released
 topDir = File.dirname(File.expand_path(__FILE__));
 $order = {
   :output_dir => File.join(topDir, "dist"),
@@ -19,11 +16,11 @@ $order = {
                 ],
   :verbose => true,
   :use_source => {
-        "bp-file"=>File.join(topDir, "bp-file")
-   },
+    "bp-file"=>File.join(topDir, "bp-file")
+  },
   :use_recipe => {
-        "bp-file"=>File.join(topDir, "bp-file", "recipe.rb"),
-   }
+    "bp-file"=>File.join(topDir, "bp-file", "recipe.rb"),
+  }
 }
 
 b = Bakery.new $order

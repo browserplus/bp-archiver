8ade4524e0136b8fd46fd27beff7152c86d5d869
Gary MacDonald <garymd@yahoo-inc.com>
2011-04-25 16:50:05 -0700

    dev

--- src/service.cpp
+++ src/service.cpp
@@ -284,7 +284,7 @@ Archiver::archive(const Transaction& tran,
         reset();
 
         // get our temp dir where archive will be created
-        bplus::tPathString tmpDir = tempDir();
+        string tmpDir = tempDir();
         if (tmpDir.empty()) {
             throw string("no temp_dir in service context");
         }
@@ -502,7 +502,7 @@ Archiver::unarchive(const Transaction& tran,
         reset();
 
         // get our temp dir where archive will be extracted
-        bplus::tPathString tmpDir = tempDir();
+        string tmpDir = tempDir();
         if (tmpDir.empty()) {
             throw string("no temp_dir in service context");
         }
